3cfc2af09f0d5d9636d3c25d6416cc20fd2e8c61
NOEL M ABY <noelmanjayilaby@gmail.com>
2023-11-08 19:37:12 +0530

    created signup pages adn setted the route

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('mainpage/login');
 });
+
+router.get('/signup',(req,res)=>{
+  res.render('mainpage/signup')
+})
 
 module.exports = router;
